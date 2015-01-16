deb95e5cdbabba43a0c2944190ccd909b4323631
Agatio <sebalandre@gmail.com>
2015-01-16 16:20:47 +0100

    Ajout de la Google Map

--- src/com/example/CultureLoisirs/MainActivity.java
+++ src/com/example/CultureLoisirs/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.CultureLoisirs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends Activity {
+
+    private GoogleMap map;
+
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +16,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
     }
 }
